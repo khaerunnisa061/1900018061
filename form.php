2075048caa73aa203a1c331dc93c274bd9e02016
khaerunnisa061a <knisaulmardiah>
2020-07-09 12:22:15 +0700

    FINAL Project : Tambah tampilan

--- form.php
+++ form.php
@@ -1,35 +1,10 @@
-<a href="form.html">kembali ke form</a>
 <?php
-echo"<pre>";
-print_r($_POST);
-echo "</pre>";
+	echo "<head><title>Form Transaksi Penjualan online</title></head>";
+	$fp =fopen ("transaksi.txt","a+");
+	fputs($fp,"$Nama|$Alamat|$Jenis_barang|$Telp|$Email|$jumlah|$harga|\n");
+	fclose($fp);
 
-$error = false;
-
-if (@$_POST['nama']=='') {
-     echo "nama tidak boleh kosong</br>";
-     $error = true;
-}
-if (@$_POST['almt']=='') {
-     echo "alamat tidak boleh kosong</br>";
-     
-}
-if(strlen(@$_POST['telpon'])<10){
-	echo "nomor telpon harus lebih dari 9 digit<br?>";
-	$error = true;
-}
-if (@$_POST['umr']=='') {
-     echo "umur tidak boleh kosong</br>";
-     
-}
-if (@$_POST['email']=='') {
-     echo "email tidak boleh kosong</br>";
-     $error = true;
-}
-
-
-if(!$error){
-	echo "trimaksih data sudah benar";
-
-}
+	echo "terimaksih atas partisipasi anda mengisi buku tamu<br>";
+	echo "<a href=form.html>isi buku tmu </a><br>";
+	echo "<a href=table.html>lihat buku tamu</a><br>";
 ?>
